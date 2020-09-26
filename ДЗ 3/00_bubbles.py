0ab589f153417e84f21877e42783a33f5def572f
Antipin <halfbonk@mail.ru>
2020-09-26 11:03:37 +0500

    Сделал 100 рандомных пузырьков с рандомным радиусом, рандомной толщиной и рандомным цветом

--- ДЗ 3/00_bubbles.py
+++ ДЗ 3/00_bubbles.py
@@ -7,11 +7,11 @@ sd.resolution = (1900, 1000)
 
 # Написать функцию рисования пузырька, принммающую 2 (или более) параметра: точка рисовании и шаг
 
-def bubble(point, step):
+def bubble(point, step, width, color):
     radius = 50
     for _ in range(3):
         radius += step
-        sd.circle(center_position=point, radius=radius, width=3, color=(255, 0, 0))
+        sd.circle(center_position=point, radius=radius, width=width, color=color)
 
 # for y in range(100, 301, 100):
 #     for x in range(100, 1001, 100):
@@ -23,5 +23,7 @@ def bubble(point, step):
 for _ in range(100):
     point = sd.random_point()
     step = random.randint(2, 10)
-    bubble(point, step)
+    width = random.randint(2, 6)
+    color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+    bubble(point, step, width, color)
 sd.pause()
