a6e50a3188531c8869f5a4d6c303eefafd21ec33
Antipin <halfbonk@mail.ru>
2020-09-26 10:59:48 +0500

    Сделал 100 рандомных пузырьков с рандомным радиусом

--- ДЗ 3/00_bubbles.py
+++ ДЗ 3/00_bubbles.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
-
+import random
 import simple_draw as sd
 
-sd.resolution = (1200, 600)
+sd.resolution = (1900, 1000)
 
 
 # Написать функцию рисования пузырька, принммающую 2 (или более) параметра: точка рисовании и шаг
@@ -11,17 +11,17 @@ def bubble(point, step):
     radius = 50
     for _ in range(3):
         radius += step
-        sd.circle(center_position=point, radius=radius, width=2)
+        sd.circle(center_position=point, radius=radius, width=3, color=(255, 0, 0))
 
-for y in range(100, 301, 100):
-    for x in range(100, 1001, 100):
-        point = sd.get_point(x, y)
-        bubble(point, 5)
-
-# Нарисовать три ряда по 10 пузырьков
-# TODO здесь ваш код
+# for y in range(100, 301, 100):
+#     for x in range(100, 1001, 100):
+#         point = sd.get_point(x, y)
+#         bubble(point, 5)
 
 # Нарисовать 100 пузырьков в произвольных местах экрана случайными цветами
 # TODO здесь ваш код
-
+for _ in range(100):
+    point = sd.random_point()
+    step = random.randint(2, 10)
+    bubble(point, step)
 sd.pause()
