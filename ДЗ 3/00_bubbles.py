735283d915245988919c1c8437696ed9fef55d3f
Antipin <halfbonk@mail.ru>
2020-09-26 10:45:24 +0500

    Сделал 10 пузырьков в ряд

--- ДЗ 3/00_bubbles.py
+++ ДЗ 3/00_bubbles.py
@@ -4,11 +4,17 @@ import simple_draw as sd
 
 sd.resolution = (1200, 600)
 
-# Нарисовать пузырек - три вложенных окружностей с шагом 5 пикселей
-# TODO здесь ваш код
-
 # Написать функцию рисования пузырька, принммающую 2 (или более) параметра: точка рисовании и шаг
-# TODO здесь ваш код
+
+def bubble(point, step):
+    radius = 50
+    for _ in range(3):
+        radius += step
+        sd.circle(center_position=point, radius=radius, width=2)
+
+for x in range(100, 1001, 100):
+    point = sd.get_point(x, 100)
+    bubble(point, 5)
 
 # Нарисовать 10 пузырьков в ряд
 # TODO здесь ваш код
