008eead599804d42ec1d9459c08ae730e7667321
Antipin <halfbonk@mail.ru>
2020-09-26 16:40:05 +0500

    Выполнил 7-е задание 3 урока. Нарисовал стену из кирпичей

--- ДЗ 3/07_wall.py
+++ ДЗ 3/07_wall.py
@@ -1,11 +1,31 @@
 # -*- coding: utf-8 -*-
 
 # (цикл for)
-import simple_draw
+import simple_draw as sd
 
+sd.resolution = (1400, 800)
 # Нарисовать стену из кирпичей. Размер кирпича - 100х50
 # Использовать вложенные циклы for
 
-# TODO здесь ваш код
 
-simple_draw.pause()
+width_line = 3
+brick_dimensions = (100, 50)
+color_brick = sd.COLOR_DARK_RED
+
+
+def brick(start_p, end_p, color, width):
+    sd.rectangle(start_p, end_p, color, width)
+
+
+for x in range(0, sd.resolution[0], 100):
+    for y in range(0, sd.resolution[1], 100):
+        start_point = sd.get_point(x, y)
+        end_point = sd.get_point(x + brick_dimensions[0], y + brick_dimensions[1])
+        brick(start_point, end_point, color_brick, width_line)
+
+    for y in range(50, sd.resolution[1], 100):
+        start_point = sd.get_point(x + 50, y)
+        end_point = sd.get_point(x + brick_dimensions[0] + 50, y + brick_dimensions[1])
+        brick(start_point, end_point, color_brick, width_line)
+
+sd.pause()
