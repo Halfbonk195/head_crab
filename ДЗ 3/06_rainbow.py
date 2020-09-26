5c7a98387838b0505d9a5e10b96be2a1e9707d3c
Antipin <halfbonk@mail.ru>
2020-09-26 15:36:27 +0500

    Выполнил 6-е задание 3 урока. Нарисовал радугу линиями

--- ДЗ 3/06_rainbow.py
+++ ДЗ 3/06_rainbow.py
@@ -1,14 +1,25 @@
 # -*- coding: utf-8 -*-
 
 # (цикл for)
-
 import simple_draw as sd
-
+sd.resolution = (1200, 600)
 rainbow_colors = (sd.COLOR_RED, sd.COLOR_ORANGE, sd.COLOR_YELLOW, sd.COLOR_GREEN,
                   sd.COLOR_CYAN, sd.COLOR_BLUE, sd.COLOR_PURPLE)
 
+
+
 # Нарисовать радугу: 7 линий разного цвета толщиной 4 с шагом 5 из точки (50, 50) в точку (350, 450)
-# TODO здесь ваш код
+start_point = sd.get_point(50, 50)
+end_point = sd.get_point(350, 450)
+
+radius = 500
+width = 10
+step = 11
+for color in rainbow_colors:
+    sd.line(start_point, end_point, color, width)
+    start_point = sd.get_point(start_point.x + step, start_point.y)
+    end_point = sd.get_point(end_point.x + step, end_point.y)
+
 
 # Усложненное задание, делать по желанию.
 # Нарисовать радугу дугами от окружности (cсм sd.circle) за нижним краем экрана,
