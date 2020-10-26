0adc864f6073cd41117dd4a58059066452e7c5d6
Antipin <halfbonk@mail.ru>
2020-10-26 09:56:46 +0500

    Просмотрел 5 блок, и выполнил 1-е упражнение

---
+++ lesson_005/01_inhabitants.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+# Вывести на консоль жителей комнат (модули room_1 и room_2)
+# Формат: В комнате room_1 живут: ...
+
+import room_1
+import room_2
+
+print('В комнате room_1 живут: ')
+for _ in room_1.folks:
+    print(_)
+
+print('В комнате room_2 живут: ')
+for _ in room_2.folks:
+    print(_)
+
+
+
+
