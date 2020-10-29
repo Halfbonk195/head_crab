99b7e312165d2012ad2deeb10fb01ef7211648d9
Antipin <halfbonk@mail.ru>
2020-10-29 12:48:09 +0500

    Изменил алгоритм постройки кирпичной стены, добавил переменную сдвига нечетного ряда

--- lesson_005/epochal_picture/wall.py
+++ lesson_005/epochal_picture/wall.py
@@ -13,15 +13,11 @@ def brick(start_p, end_p, brick_dimensions, color=sd.COLOR_YELLOW, width=1):
         end_p - верхний правый угол стены, тип (x, y)
         brick_dimensions - размер кирпича (x, y)
     """
-
-    for x in range(start_p[0], end_p[0], brick_dimensions[0]):
-        for y in range(start_p[1], end_p[1], brick_dimensions[1] * 2):
-            start_point = sd.get_point(x, y)
-            end_point = sd.get_point(x + brick_dimensions[0], y + brick_dimensions[1])
+    row_shift = 0
+    for y in range(start_p[1], end_p[1], brick_dimensions[1]):
+        row_shift += 1
+        for x in range(start_p[0], end_p[0], brick_dimensions[0]):
+            x0 = x if row_shift % 2 else x + brick_dimensions[0] // 2
+            start_point = sd.get_point(x0, y)
+            end_point = sd.get_point(x0 + brick_dimensions[0], y + brick_dimensions[1])
             sd.rectangle(start_point, end_point, color, width)
-
-        for y in range(start_p[1] + brick_dimensions[1], end_p[1], brick_dimensions[1] * 2):
-            start_point = sd.get_point(x + brick_dimensions[0] // 2, y)
-            end_point = sd.get_point(x + brick_dimensions[0] + brick_dimensions[0] // 2, y + brick_dimensions[1])
-            sd.rectangle(start_point, end_point, color, width)
-
