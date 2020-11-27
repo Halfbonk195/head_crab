60dfd740f9f095a668774d0f4d5b21383f640520
Antipin <halfbonk@mail.ru>
2020-11-27 11:47:25 +0500

    Решение системыв нелинейных уравнений методом Ньютона v2.0

---
+++ calculation/newton_method/newton_engine.py
@@ -0,0 +1,83 @@
+from sympy import symbols, diff, lambdify
+import numpy as np
+
+_diff_dict = {}
+_diff_numeric = {}
+_solution_linear_eqs = []
+
+def find_partial_diff(f1, f2):
+    """Находит частные производные по 2-м функциям и записывает их в словарь"""
+    global _diff_dict
+    _diff_dict = {}
+
+    x, y = symbols('x, y')
+    _diff_dict['f1'] = f1
+    _diff_dict['f2'] = f2
+    _diff_dict['df1x'] = diff(f1, x)
+    _diff_dict['df2x'] = diff(f2, x)
+    _diff_dict['df1y'] = diff(f1, y)
+    _diff_dict['df2y'] = diff(f2, y)
+
+    return _diff_dict
+
+
+def find_partial_diff_numeric(x_num, y_num):
+    """Конвертирует символьные производные в норальные функции
+       и находит численное значение в точке (x_num, y_num)
+       записывает в _diff_numeric"""
+
+    global _diff_numeric
+    _diff_numeric = {}
+    x, y = symbols('x, y')
+    for key in _diff_dict:
+        f = _diff_dict[key]
+        f = lambdify([x, y], f, 'numpy')
+        _diff_numeric[key] = f(x_num, y_num)
+
+    return _diff_numeric
+
+
+def write_matrix():
+    matrix = [[], []]
+    matrix[0].append(_diff_numeric['df1x'])
+    matrix[0].append(_diff_numeric['df1y'])
+    matrix[0].append(-_diff_numeric['f1'])
+    matrix[1].append(_diff_numeric['df2x'])
+    matrix[1].append(_diff_numeric['df2y'])
+    matrix[1].append(-_diff_numeric['f2'])
+    return matrix
+
+
+def solve_linear_eqs(matrix):
+    """Решает систему из 2-х линейных уравнений"""
+    global _solution_linear_eqs
+    _solution_linear_eqs = []
+
+    delta = matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
+    delta_x = matrix[0][2] * matrix[1][1] - matrix[0][1] * matrix[1][2]
+    delta_y = matrix[0][0] * matrix[1][2] - matrix[0][2] * matrix[1][0]
+
+    _solution_linear_eqs.append(delta_x/delta)
+    _solution_linear_eqs.append(delta_y/delta)
+    return None
+
+
+def newton_method(x, y, eps):
+    x1 = x + 1
+    y1 = y + 1
+    counter_iter = 0
+    while abs(x1 - x) >= eps and abs(y1 - y) >= eps:
+        x1 = x
+        y1 = y
+        find_partial_diff_numeric(x, y)
+        matrix = write_matrix()
+        solve_linear_eqs(matrix)
+
+        g_n = _solution_linear_eqs[0]
+        h_n = _solution_linear_eqs[1]
+
+        x += g_n
+        y += h_n
+        counter_iter += 1
+
+    return x, y, counter_iter
--- calculation/newton_method/newton_method.py
+++ calculation/newton_method/newton_method.py
@@ -1,61 +1,24 @@
-import sys
-import math, numpy
-from sympy import symbols, tanh, diff, lambdify, cos, sin
-import matplotlib.pyplot as plt
-from matplotlib.ticker import (MultipleLocator, FormatStrFormatter,
-                               AutoMinorLocator)
+from newton_engine import find_partial_diff, newton_method
+from sympy import symbols, cos, sin
 
 
 def f1(x, y):
-    return cos(y+0.5) +x -0.8
+    return sin(2*x - y) - 1.2*x - 0.4
 
 
 def f2(x, y):
-    return sin(x) - 2*y - 1.6
+    return 0.8 * x**2 + 1.5 * y**2 - 1
 
 
-def diff_f1x(x, y):
-    return 1
+x_symb, y_symb = symbols('x, y')
+diff_dict = find_partial_diff(f1(x_symb, y_symb), f2(x_symb, y_symb))
+for key in diff_dict:
+    print(key, '=', diff_dict[key])
 
+x_num = 0.4
+y_num = -0.75
+n_eps = 4
+eps = 0.1 ** n_eps
 
-def diff_f1y(x, y):
-    return -sin(y + 0.5)
-
-
-def diff_f2x(x, y):
-    return cos(x)
-
-
-def diff_f2y(x, y):
-    return -2
-
-
-
-def Newton(x, y, eps):
-    x1 = x * 100
-    y1 = y * 100
-    counter_iter = 0
-    while abs(x1 - x) > eps and abs(y1 - y) > eps:
-        x1 = x
-        y1 = y
-        delta = diff_f1x(x, y)*diff_f2y(x, y) - diff_f1y(x, y)*diff_f2x(x, y)
-        delta_x = -f1(x, y)*diff_f2y(x, y) + diff_f1y(x,y)*f2(x, y)
-        delta_y = -diff_f1x(x, y)*f2(x, y) + f1(x, y)*diff_f2x(x, y)
-
-        g_n = delta_x/delta
-        h_n = delta_y/delta
-
-        x += g_n
-        y += h_n
-        counter_iter += 1
-
-    return x, y, counter_iter
-
-
-x = -0.1
-y = -0.8
-eps = 0.0001
-solv = Newton(x=x, y=y, eps=eps)
-
-print('x = {}, y = {}, i = {}'.format(round(solv[0], 3), round(solv[1], 3), solv[2]))
-
+x_end, y_end, iteration = newton_method(x_num, y_num, eps)
+print('\nОтвет: x =', round(x_end, n_eps), 'y =', round(y_end, n_eps), 'количество итераций =', iteration)
