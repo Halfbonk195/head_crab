6c77167101b3b1d213df9b8c7f48408487f71790
halfb <halfbonk@gmil.com>
2020-12-06 17:19:08 +0500

    Снежика как класс, падает 1 снежинка

--- lesson_007/01_snowfall.py
+++ lesson_007/01_snowfall.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import simple_draw as sd
-
+sd.resolution = (1200, 800)
 # Шаг 1: Реализовать падение снежинки через класс. Внести в методы:
 #  - создание снежинки с нужными параметрами
 #  - отработку изменений координат
@@ -9,9 +9,26 @@ import simple_draw as sd
 
 
 class Snowflake:
-    pass
+    """Снежинка"""
 
-    # TODO здесь ваш код
+    def __init__(self):
+        self.length_flake = sd.random_number(10, 100)
+        self.x = sd.random_number(50, 1150)
+        self.y = 800
+
+    def clear_previous_picture(self):
+        sd.clear_screen()
+
+    def move(self):
+        self.x += sd.random_number(-10, 10)
+        self.y -= 10
+
+    def draw(self):
+        point = sd.get_point(self.x, self.y)
+        sd.snowflake(center=point, length=self.length_flake)
+
+    def can_fall(self):
+        return self.y > self.length_flake
 
 
 flake = Snowflake()
@@ -20,6 +37,7 @@ while True:
     flake.clear_previous_picture()
     flake.move()
     flake.draw()
+
     if not flake.can_fall():
         break
     sd.sleep(0.1)
