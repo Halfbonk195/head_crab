fce19f08555b4f7f06ef0341e27dcd9347c314fd
halfb <halfbonk@gmil.com>
2020-12-17 22:37:43 +0500

    2 ДЗ переделанная на норм рабочую версию

--- lesson_009/02_log_parser.py
+++ lesson_009/02_log_parser.py
@@ -20,6 +20,7 @@
 # Требования к коду: он должен быть готовым к расширению функциональности. Делать сразу на классах.
 import time
 
+
 file_name_r = 'events.txt'
 file_name_w = 'sorted_events.txt'
 
@@ -29,48 +30,28 @@ with open(file_name_r, 'r') as file_r:
         old_line = None
         count_NOK = 0
         count_OK = 0
+        current_minute = None
+
         for line in file_r:
-            new_line = ''
-            CUT = False
-            for char in line:
-                if char == ']':
-                    CUT = False
+            new_line = line[1:20] + line[28:]
 
-                if char == '.' or CUT:
-                    CUT = True
-                    continue
+            time_1 = time.strptime(new_line[0:19], "%Y-%m-%d %H:%M:%S")
+            if current_minute is None:
+                current_minute = time_1.tm_min
+
+            if current_minute == time_1.tm_min:
+                if 'NOK' in new_line:
+                    count_NOK += 1
+            else:
+                file_w.write(f'[{old_line[:17]}] {count_NOK}\n')
+                if 'NOK' in new_line:
+                    count_NOK = 1
                 else:
-                    new_line += char
-
-            eq = False
-            if old_line:
-                time_1 = time.strptime(new_line[1:20], "%Y-%m-%d %H:%M:%S")
-                time_2 = time.strptime(old_line[1:20], "%Y-%m-%d %H:%M:%S")
-
-                if time_1.tm_min == time_2.tm_min:
-                    if new_line[-3:] == old_line[-3:]:
-                        if new_line[-4:-1] == 'NOK':
-                            count_NOK += 1
-                        else:
-                            count_OK += 1
-                        eq = True
-                if eq:
-                    pass
-                else:
-                    if count_NOK:
-                        print(f'{old_line[0:17]}] {count_NOK}')
-                        count_NOK = 0
-            elif new_line[-4:-1] == 'NOK':
-                count_NOK = 1
+                    count_NOK = 0
+                current_minute = time_1.tm_min
             old_line = new_line
 
-            result_time = time.strptime(new_line[1:20], "%Y-%m-%d %H:%M:%S")
-            time_start = result_time
 
-            quantity_OK = 0
-
-            # file_w.write(new_line)
-            # print(new_line)
 
 # После выполнения первого этапа нужно сделать группировку событий
 #  - по часам
