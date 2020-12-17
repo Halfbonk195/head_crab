b0e76fbcc7c58c4fe28df33f851ef950d9a23b57
Antipin <halfbonk@mail.ru>
2020-12-17 19:15:37 +0500

    lesson 9 - 1 дз второй вариант на классах

--- lesson_009/01_char_stat.py
+++ lesson_009/01_char_stat.py
@@ -22,40 +22,54 @@
 # Требования к коду: он должен быть готовым к расширению функциональности. Делать сразу на классах.
 from pprint import pprint
 
+
+class Stats_letters():
+
+    def __init__(self):
+        self.collector = {}
+        self.sorted_collection = []
+        self.quantity = 0
+
+    def collect(self, f_name):
+
+        with open(f_name, 'r', encoding='cp1251') as file:
+            for line in file:
+                for char in line:
+                    if char.isalpha():
+                        if char in self.collector:
+                            self.collector[char] += 1
+                        else:
+                            self.collector[char] = 1
+                    else:
+                        continue
+
+    def sort(self):
+        for key, value in self.collector.items():
+            self.sorted_collection.append([value, key])
+        self.sorted_collection.sort(reverse=True)
+
+    def output(self):
+        print(f'+{"-" * 9}+{"-" * 9}+')
+        print(f'|{"Буква":^9}|{"Частота":^9}|')
+        print(f'+{"-" * 9}+{"-" * 9}+')
+
+        for element in self.sorted_collection:
+            frequency = element[0]
+            char = element[1]
+            self.quantity += frequency
+            print(f'|{char:^9}|{frequency:^9}|')
+        print(f'+{"-" * 9}+{"-" * 9}+')
+
+        print(f'|{"Итого:":^9}|{self.quantity:^9}|')
+        print(f'+{"-" * 9}+{"-" * 9}+')
+
+
 file_name = 'voyna-i-mir.txt'
 
-collector = {}
-with open(file_name, 'r', encoding='cp1251') as file:
-    for line in file:
-        for char in line:
-            if char.isalpha():
-                if char in collector:
-                    collector[char] += 1
-                else:
-                    collector[char] = 1
-            else:
-                continue
-
-sorted_collection = []
-
-for key, value in collector.items():
-    sorted_collection.append([value, key])
-sorted_collection.sort(reverse=True)
-
-print(f'+{"-" * 9}+{"-" * 9}+')
-print(f'|{"Буква":^9}|{"Частота":^9}|')
-print(f'+{"-" * 9}+{"-" * 9}+')
-
-quantity = 0
-for element in sorted_collection:
-    frequency = element[0]
-    char = element[1]
-    quantity += frequency
-    print(f'|{char:^9}|{frequency:^9}|')
-print(f'+{"-" * 9}+{"-" * 9}+')
-
-print(f'|{"Итого:":^9}|{quantity:^9}|')
-print(f'+{"-" * 9}+{"-" * 9}+')
+stat_1 = Stats_letters()
+stat_1.collect(f_name=file_name)
+stat_1.sort()
+stat_1.output()
 
 # После выполнения первого этапа нужно сделать упорядочивание статистики
 #  - по частоте по возрастанию
