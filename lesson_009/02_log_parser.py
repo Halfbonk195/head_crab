5474e8d07645a144c81fb49c042a318b33115a5c
Antipin <halfbonk@mail.ru>
2020-12-17 21:24:27 +0500

    2 урок первая говно рабочая версия

--- lesson_009/02_log_parser.py
+++ lesson_009/02_log_parser.py
@@ -18,8 +18,59 @@
 #
 # Входные параметры: файл для анализа, файл результата
 # Требования к коду: он должен быть готовым к расширению функциональности. Делать сразу на классах.
+import time
 
-# TODO здесь ваш код
+file_name_r = 'events.txt'
+file_name_w = 'sorted_events.txt'
+
+with open(file_name_r, 'r') as file_r:
+    with open(file_name_w, 'w') as file_w:
+
+        old_line = None
+        count_NOK = 0
+        count_OK = 0
+        for line in file_r:
+            new_line = ''
+            CUT = False
+            for char in line:
+                if char == ']':
+                    CUT = False
+
+                if char == '.' or CUT:
+                    CUT = True
+                    continue
+                else:
+                    new_line += char
+
+            eq = False
+            if old_line:
+                time_1 = time.strptime(new_line[1:20], "%Y-%m-%d %H:%M:%S")
+                time_2 = time.strptime(old_line[1:20], "%Y-%m-%d %H:%M:%S")
+
+                if time_1.tm_min == time_2.tm_min:
+                    if new_line[-3:] == old_line[-3:]:
+                        if new_line[-4:-1] == 'NOK':
+                            count_NOK += 1
+                        else:
+                            count_OK += 1
+                        eq = True
+                if eq:
+                    pass
+                else:
+                    if count_NOK:
+                        print(f'{old_line[0:17]}] {count_NOK}')
+                        count_NOK = 0
+            elif new_line[-4:-1] == 'NOK':
+                count_NOK = 1
+            old_line = new_line
+
+            result_time = time.strptime(new_line[1:20], "%Y-%m-%d %H:%M:%S")
+            time_start = result_time
+
+            quantity_OK = 0
+
+            # file_w.write(new_line)
+            # print(new_line)
 
 # После выполнения первого этапа нужно сделать группировку событий
 #  - по часам
