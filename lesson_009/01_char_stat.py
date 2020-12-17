d05506233c505a47e079648dd051529e81ff880b
Antipin <halfbonk@mail.ru>
2020-12-17 19:01:52 +0500

    lesson 9 - 1 дз первый вариант

--- lesson_009/01_char_stat.py
+++ lesson_009/01_char_stat.py
@@ -20,8 +20,42 @@
 #
 # Упорядочивание по частоте - по убыванию. Ширину таблицы подберите по своему вкусу
 # Требования к коду: он должен быть готовым к расширению функциональности. Делать сразу на классах.
+from pprint import pprint
 
-# TODO здесь ваш код
+file_name = 'voyna-i-mir.txt'
+
+collector = {}
+with open(file_name, 'r', encoding='cp1251') as file:
+    for line in file:
+        for char in line:
+            if char.isalpha():
+                if char in collector:
+                    collector[char] += 1
+                else:
+                    collector[char] = 1
+            else:
+                continue
+
+sorted_collection = []
+
+for key, value in collector.items():
+    sorted_collection.append([value, key])
+sorted_collection.sort(reverse=True)
+
+print(f'+{"-" * 9}+{"-" * 9}+')
+print(f'|{"Буква":^9}|{"Частота":^9}|')
+print(f'+{"-" * 9}+{"-" * 9}+')
+
+quantity = 0
+for element in sorted_collection:
+    frequency = element[0]
+    char = element[1]
+    quantity += frequency
+    print(f'|{char:^9}|{frequency:^9}|')
+print(f'+{"-" * 9}+{"-" * 9}+')
+
+print(f'|{"Итого:":^9}|{quantity:^9}|')
+print(f'+{"-" * 9}+{"-" * 9}+')
 
 # После выполнения первого этапа нужно сделать упорядочивание статистики
 #  - по частоте по возрастанию
