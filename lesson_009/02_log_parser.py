11264b6be01f4a28d0f890cc5f0fb2f8b987a54b
Antipin <halfbonk@mail.ru>
2020-12-19 15:55:26 +0500

    2 ДЗ на классах v1

--- lesson_009/02_log_parser.py
+++ lesson_009/02_log_parser.py
@@ -21,37 +21,47 @@
 import time
 
 
-file_name_r = 'events.txt'
-file_name_w = 'sorted_events.txt'
+class LogParser:
 
-with open(file_name_r, 'r') as file_r:
-    with open(file_name_w, 'w') as file_w:
+    def __init__(self, file_name):
+        self.file_name_r = file_name
+        self.old_line = None
+        self.count_NOK = 0
+        self.count_OK = 0
+        self.current_minute = None
 
-        old_line = None
-        count_NOK = 0
-        count_OK = 0
-        current_minute = None
+    def action(self):
+        file_name_w = 'sorted_' + self.file_name_r
+        with open(self.file_name_r, 'r') as file_r:
+            with open(file_name_w, 'w') as file_w:
+                self.read_lines(file_r, file_w)
 
+    def read_lines(self, file_r, file_w):
         for line in file_r:
             new_line = line[1:20] + line[28:]
-
             time_1 = time.strptime(new_line[0:19], "%Y-%m-%d %H:%M:%S")
-            if current_minute is None:
-                current_minute = time_1.tm_min
 
-            if current_minute == time_1.tm_min:
+            if self.current_minute is None:
+                self.current_minute = time_1.tm_min
+
+            if self.current_minute == time_1.tm_min:
                 if 'NOK' in new_line:
-                    count_NOK += 1
+                    self.count_NOK += 1
             else:
-                file_w.write(f'[{old_line[:17]}] {count_NOK}\n')
+                time_old_struct = time.strptime(self.old_line[0:19], "%Y-%m-%d %H:%M:%S")
+                time_old_str = time.strftime("%Y-%m-%d %H:%M:%S", time_old_struct)
+                file_w.write(f'[{time_old_str}] {self.count_NOK}\n')
                 if 'NOK' in new_line:
-                    count_NOK = 1
+                    self.count_NOK = 1
                 else:
-                    count_NOK = 0
-                current_minute = time_1.tm_min
-            old_line = new_line
+                    self.count_NOK = 0
+                self.current_minute = time_1.tm_min
+            self.old_line = new_line
 
 
+file_name_r = 'events.txt'
+log_parser = LogParser(file_name_r)
+log_parser.action()
 
 # После выполнения первого этапа нужно сделать группировку событий
 #  - по часам
