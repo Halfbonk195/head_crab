ec567a62632cdf32481642025f6029bbe41d1f85
Antipin <halfbonk@mail.ru>
2020-12-20 12:21:33 +0500

    3 ДЗ выполнено усложненное задание на сортировку файлов по дате из zip файла

--- lesson_009/03_files_arrange.py
+++ lesson_009/03_files_arrange.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import os, time, shutil
+import zipfile
 
 # Нужно написать скрипт для упорядочивания фотографий (вообще любых файлов)
 # Скрипт должен разложить файлы из одной папки по годам и месяцам в другую.
@@ -39,13 +40,19 @@ from pprint import pprint
 class SortMyPhoto:
 
     def __init__(self, dir_for_scan, dir_for_copy):
-        self.dir_read = os.path.normpath(dir_for_scan)
+        if dir_for_scan is str:
+            self.dir_read = os.path.normpath(dir_for_scan)
         self.dir_write = os.path.normpath(dir_for_copy)
         self.files_dict = {}
         self.sorted_files_dict = {}
 
     def __str__(self):
         return 'Я сортирую фотки по годам и месяцам'
+
+    def action(self):
+        self.scan_dir()
+        self.sort_files()
+        self.copy_files()
 
     def scan_dir(self):
         """Сканирует дирикторию и запоминает пути и дату до файлов"""
@@ -89,16 +96,56 @@ class SortMyPhoto:
 
                 for file_name in file_names:
                     src_path = self.files_dict[file_name]['path']
-                    shutil.copy2(src_path, dir_path)
+                    self._copy(src_path, dir_path)
+
+    def _copy(self, src_path, dir_path):
+        shutil.copy2(src_path, dir_path)
 
 
-path_for_scan = 'C:/Users/halfb/Desktop/Test'
-path_for_copy = 'C:/Users/halfb/Desktop/Test/sort'
+class SortMyPhotoZip(SortMyPhoto):
 
-sort_my_files = SortMyPhoto(path_for_scan, path_for_copy)
-sort_my_files.scan_dir()
-sort_my_files.sort_files()
-sort_my_files.copy_files()
+    def __init__(self, path_to_zip_file, dir_for_copy):
+        super().__init__(dir_for_scan=None, dir_for_copy=dir_for_copy)
+        self.path_to_zip_file = os.path.normpath(path_to_zip_file)
+
+    def action(self):
+        self.scan_zip()
+        self.sort_files()
+        self.copy_files()
+
+    def scan_zip(self):
+        if zipfile.is_zipfile(self.path_to_zip_file):
+            z_file = zipfile.ZipFile(self.path_to_zip_file)
+
+            for file in z_file.namelist():
+                info_file = z_file.getinfo(file)
+                name = os.path.basename(info_file.filename)
+                if name == '':
+                    continue
+                full_path = info_file.filename
+                file_time = time.strptime(str(info_file.date_time), "(%Y, %m, %d, %H, %M, %S)")
+                year = time.strftime('%Y', file_time)
+                month = time.strftime('%m', file_time)
+
+                self.files_dict[name] = {'path': full_path, 'year': year, 'month': month, }
+
+    def _copy(self, dir_zip, dir_unzip):
+        zip_file = zipfile.ZipFile(self.path_to_zip_file, 'r')
+        zip_file.extract(dir_zip, path=dir_unzip)
+
+
+path_to_zip = 'icons.zip'
+path_for_copy = 'sorted_icons'
+sort_my_files = SortMyPhotoZip(path_to_zip, path_for_copy)
+sort_my_files.action()
+
+# path_for_scan = 'C:/Users/halfb/Desktop/Test'
+# path_for_copy = 'C:/Users/halfb/Desktop/Test/sort'
+#
+# sort_my_files = SortMyPhoto(path_for_scan, path_for_copy)
+# sort_my_files.scan_dir()
+# sort_my_files.sort_files()
+# sort_my_files.copy_files()
 
 # Усложненное задание (делать по желанию)
 # Нужно обрабатывать zip-файл, содержащий фотографии, без предварительного извлечения файлов в папку.
