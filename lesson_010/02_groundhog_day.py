2e9358f616eeb24b56fe87b345ed81b9b14e9db3
Antipin <halfbonk@mail.ru>
2021-02-11 10:48:44 +0500

    1 рабочая версия - скелет

--- lesson_010/02_groundhog_day.py
+++ lesson_010/02_groundhog_day.py
@@ -3,6 +3,25 @@
 # День сурка
 #
 # Напишите функцию one_day() которая возвращает количество кармы от 1 до 7
+from random import randint
+
+ENLIGHTENMENT_CARMA_LEVEL = 77
+
+
+def one_day():
+    carma = randint(1, 7)
+    return carma
+
+
+sum_carma = 0
+day = 0
+while True:
+    day += 1
+    sum_carma += one_day()
+    print(f'День {day}, количество кармы {sum_carma}')
+    if sum_carma >= ENLIGHTENMENT_CARMA_LEVEL:
+        break
+
 # и может выкидывать исключения:
 # - IamGodError
 # - DrunkError
@@ -17,7 +36,7 @@
 # При создании собственных исключений максимально использовать функциональность
 # базовых встроенных исключений.
 
-ENLIGHTENMENT_CARMA_LEVEL = 777
+7
 
 # TODO здесь ваш код
 
