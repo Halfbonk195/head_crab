30ddd176ff194e6a5a2b392c48dd35ff2a36f515
Antipin <halfbonk@mail.ru>
2021-04-07 13:54:37 +0500

    3 дз из 10 урока сделаль

--- lesson_010/03_registration_log.py
+++ lesson_010/03_registration_log.py
@@ -21,5 +21,51 @@
 # - поле емейл НЕ содержит @ и .(точку): NotEmailError (кастомное исключение)
 # - поле возраст НЕ является числом от 10 до 99: ValueError
 # Вызов метода обернуть в try-except.
+class NotNameError(Exception):
 
-# TODO здесь ваш код
+    def __str__(self):
+        return 'Поле имени содержит НЕ только буквы'
+
+
+class NotEmailError(Exception):
+
+    def __str__(self):
+        return 'Поле емейл НЕ содержит @ и точку "."'
+
+
+def data_validation(line):
+    """ Проверка данных, полученных из файла """
+    try:
+        name, mail, age = line.split(' ')
+    except ValueError:
+        raise ValueError('НЕ присутсвуют все три поля')
+
+    age = int(age)
+
+    if not name.isalpha():
+        raise NotNameError()
+
+    if not ('@' in mail and '.' in mail):
+        raise NotEmailError()
+
+    if age < 10 or age > 99:
+        raise ValueError('Поле возраст НЕ является числом от 10 до 99')
+
+
+with open('registrations.txt', mode='r', encoding='utf-8') as data:
+    with open('registrations_good.log', mode='w', encoding='utf-8') as registrations_good:
+        with open('registrations_bad.log', mode='w', encoding='utf-8') as registrations_bad:
+            for line in data:
+                try:
+                    data_validation(line[:-1])
+                except NotNameError as exc:
+                    registrations_bad.write(f'{line[:-1]} - {exc}\n')
+                    continue
+                except NotEmailError as exc:
+                    registrations_bad.write(f'{line[:-1]} - {exc}\n')
+                    continue
+                except ValueError as exc:
+                    registrations_bad.write(f'{line[:-1]} - {exc}\n')
+                    continue
+
+                registrations_good.write(line)
