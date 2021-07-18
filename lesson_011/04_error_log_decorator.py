81705fe2928c87129d3b5a7df438d3ba440232a3
Antipin <halfbonk@mail.ru>
2021-07-18 15:22:04 +0500

    Выполнил 4 задание 11 урока

--- lesson_011/04_error_log_decorator.py
+++ lesson_011/04_error_log_decorator.py
@@ -7,19 +7,44 @@
 # Формат лога: <имя функции> <параметры вызова> <тип ошибки> <текст ошибки>
 # Лог файл открывать каждый раз при ошибке в режиме 'a'
 
+def log_errors(name):
+    def log_errors_inner(func):
+        def surrogate(*args, **kwargs):
+            result = None
+            massage_2 = str()
+            try:
+                result = func(*args, **kwargs)
+            except Exception as exc:
+                file = open(name, 'a', encoding='utf8')
+                massage_1 = func.__name__
+                for elem in args:
+                    massage_2 += str(elem)
+                for elem, item in kwargs.items():
+                    if massage_2:
+                        massage_2 += ', '
+                    massage_2 += elem + '=' + str(item)
+                massage_2 = '(' + massage_2 + ')'
+                massage_3 = str(type(exc))
+                massage_4 = str(exc)
 
-def log_errors(func):
-    pass
-    # TODO здесь ваш код
+                file.write(massage_1 + massage_2 + ' ' + massage_3 + ' ' + massage_4)
+                file.write('\n')
+                raise Exception(exc)
+
+            return result
+
+        return surrogate
+
+    return log_errors_inner
 
 
 # Проверить работу на следующих функциях
-@log_errors
-def perky(param):
+@log_errors('function_errors.log')
+def perky(x, param):
     return param / 0
 
 
-@log_errors
+@log_errors('function_errors.log')
 def check_line(line):
     name, email, age = line.split(' ')
     if not name.isalpha():
@@ -43,8 +68,11 @@ for line in lines:
         check_line(line)
     except Exception as exc:
         print(f'Invalid format: {exc}')
-perky(param=42)
 
+try:
+    perky(54, param=42)
+except Exception as exc:
+    print(f'Invalid format: {exc}')
 
 # Усложненное задание (делать по желанию).
 # Написать декоратор с параметром - именем файла
@@ -52,4 +80,3 @@ perky(param=42)
 # @log_errors('function_errors.log')
 # def func():
 #     pass
-
