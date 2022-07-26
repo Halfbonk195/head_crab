91f772280aeffb9a12b5e8899593d0c266bd8763
Antipin <halfbonk@gmail.com>
2022-07-26 20:34:40 +0500

    Сделал 5ый урок

---
+++ EricMatis/chapter_5/5.3.py
@@ -0,0 +1,7 @@
+alien_color = 'green'
+
+if alien_color == 'green':
+    print('Игрок заработал 5 очков!')
+
+if alien_color == 'yellow':
+    pass
---
+++ EricMatis/chapter_5/5.4.py
@@ -0,0 +1,13 @@
+alien_color = 'green'
+
+if alien_color == 'green':
+    print('Игрок заработал 5 очков!')
+else:
+    print('Игрок заработал 10 очков!')
+
+alien_color = 'yellow'
+
+if alien_color == 'green':
+    print('Игрок заработал 5 очков!')
+else:
+    print('Игрок заработал 10 очков!')
