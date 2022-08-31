3254f7e65a767410068eca8e41ef9d09e9d238d9
Antipin <halfbonk@gmail.com>
2022-08-31 21:25:30 +0500

    Сделал задание 12.6

--- EricMatis/chapter_12/settings_rocket.py
+++ EricMatis/chapter_12/settings_rocket.py
@@ -8,3 +8,8 @@ class Settings:
         self.screen_height = 990
         self.bg_color = (11, 62, 62)
         self.ship_speed = 2.5
+        self.bullet_speed = 1
+        self.bullet_width = 15
+        self.bullet_height = 3
+        self.bullet_color = (238, 241, 34)
+        self.bullet_allowed = 5
--- EricMatis/chapter_12/side_firing.py
+++ EricMatis/chapter_12/side_firing.py
@@ -3,10 +3,11 @@ import sys
 import pygame
 
 from settings_rocket import Settings
-from ship import Ship
+from side_ship import Ship
+from bullet import Bullet
 
 
-class Rocket:
+class AlienInvasion:
     """Класс для управления ресурсами и поведением игры"""
 
     def __init__(self):
@@ -20,18 +21,31 @@ class Rocket:
         pygame.display.set_caption('Alien Invasion')
 
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
 
     def run_game(self):
         """Запуск основного цикла игры"""
         while True:
             self._check_events()
             self.ship.update()
+            self._update_bullets()
             self._update_screen()
+
+    def _update_bullets(self):
+        """Обновляет позиции снарядов и уничтожает старые снаряды."""
+        # Обновление позиций снарядов.
+        self.bullets.update()
+        # Удаление снарядов, вышедших за край экрана
+        for bullet in self.bullets.copy():
+            if bullet.rect.left >= self.screen.get_rect().right:
+                self.bullets.remove(bullet)
 
     def _update_screen(self):
         """Обновляет изображения на экране и отображает новый экран."""
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
         # Отображение последнего прорисованного экрана
         pygame.display.flip()
 
@@ -49,30 +63,29 @@ class Rocket:
 
     def _check_keyup_events(self, event):
         """Реагирует на отпускание клавиш"""
-        if event.key == pygame.K_RIGHT:
-            self.ship.moving_right = False
-        elif event.key == pygame.K_LEFT:
-            self.ship.moving_left = False
-        elif event.key == pygame.K_UP:
+        if event.key == pygame.K_UP:
             self.ship.moving_up = False
         elif event.key == pygame.K_DOWN:
             self.ship.moving_down = False
 
     def _check_keydown_events(self, event):
         """Реагирует на нажатие клавиш"""
-        if event.key == pygame.K_RIGHT:
-            self.ship.moving_right = True
-        elif event.key == pygame.K_LEFT:
-            self.ship.moving_left = True
-        elif event.key == pygame.K_UP:
+        if event.key == pygame.K_UP:
             self.ship.moving_up = True
         elif event.key == pygame.K_DOWN:
             self.ship.moving_down = True
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
         elif event.key == pygame.K_q:
             sys.exit()
+
+    def _fire_bullet(self):
+        if len(self.bullets) < self.settings.bullet_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
 
 
 if __name__ == '__main__':
     # Создание экземпляра и запуск игры.
-    rocket_game = Rocket()
-    rocket_game.run_game()
+    ai = AlienInvasion()
+    ai.run_game()
