2e74c89cca4fc0182339f9dc6b389cfa9fc6234e
Antipin <halfbonk@gmail.com>
2022-08-31 20:18:24 +0500

    Ограничение количества снарядов на экране

--- EricMatis/projects/alien_invasion/alien_invasion.py
+++ EricMatis/projects/alien_invasion/alien_invasion.py
@@ -75,8 +75,9 @@ class AlienInvasion:
 
     def _fire_bullet(self):
         """Создание нового снаряда и включение его в группу bullets."""
-        new_bullet = Bullet(self)
-        self.bullets.add(new_bullet)
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
 
 
 if __name__ == '__main__':
