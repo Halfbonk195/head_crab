c47c786ffdf0e898d321c57078fd92576c10e109
Antipin <halfbonk@gmail.com>
2022-09-06 21:24:33 +0500

    Добавил класс пришельца и его картинку

---
+++ EricMatis/projects/alien_invasion_side/alien.py
@@ -0,0 +1,35 @@
+import pygame
+from pygame.sprite import Sprite
+
+
+class Alien(Sprite):
+    """Класс, представляющий одного пришельца."""
+
+    def __init__(self, ai_game):
+        """Инициализирует пришельца и задает его начальную позицию."""
+        super().__init__()
+        self.screen = ai_game.screen
+        self.settings = ai_game.settings
+
+        # Загрузка изображения пришельца и назначение атрибута rect.
+        self.image = pygame.image.load('images/alien_ship.png')
+        self.image = pygame.transform.rotate(self.image, 270)
+        self.rect = self.image.get_rect()
+
+        # Каждый новый пришелец появляется в левом верхнем углу экрана.
+        self.rect.x = self.rect.width
+        self.rect.y = self.rect.height
+
+        # Сохранение точной вертикальной позиции пришельца.
+        self.y = float(self.rect.y)
+
+    def check_edges(self):
+        """Возвращает True, если пришелец находится у края экрана."""
+        screen_rect = self.screen.get_rect()
+        if self.rect.bottom >= screen_rect.bottom or self.rect.top <= 0:
+            return True
+
+    def update(self):
+        """Перемещает пришельца вверх или вниз."""
+        self.y += (self.settings.alien_speed * self.settings.fleet_direction)
+        self.rect.y = self.y
